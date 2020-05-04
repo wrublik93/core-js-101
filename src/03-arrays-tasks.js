7c96bdaa90a4677b197006859a17390990604268
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-05-05 00:46:52 +0300

    feat: implement 53 tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -294,8 +294,10 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  /* throw new Error('Not implemented'); */
+  arr.sort((a, b) => b - a);
+  return arr.splice(0, 3);
 }
 
 
@@ -312,8 +314,10 @@ function get3TopItems(/* arr */) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  /* throw new Error('Not implemented'); */
+  const result = arr.filter((item) => typeof item === 'number' && item > 0);
+  return result.length;
 }
 
 /**
@@ -329,8 +333,21 @@ function getPositivesCount(/* arr */) {
  *   [ 'nine','eight','nine','eight'] => [ 'eight','eight','nine','nine']
  *   [ 'one','one','one','zero' ]     => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  /* throw new Error('Not implemented'); */
+  const digit = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+  };
+  return arr.sort((a, b) => digit[a] - digit[b]);
 }
 
 /**
@@ -345,10 +362,12 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   [ -1, 1, -1, 1 ]      => 0
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
-function getItemsSum(/* arr */) {
-  throw new Error('Not implemented');
-  /*  if (arr.length) {}
-  return arr.reduce((a, b) => a + b); */
+function getItemsSum(arr) {
+  /* throw new Error('Not implemented'); */
+  if (arr.length === 0) {
+    return 0;
+  }
+  return arr.reduce((a, b) => a + b);
 }
 
 /**
